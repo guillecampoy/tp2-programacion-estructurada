d17ac9bab846e88cad6d05551824f91347883d6a
Guillermo Campoy <guillecampoy@gmail.com>
2025-08-28 20:28:31 -0300

    Implemento ejercicio n2 Se desarrolla el mismo como propone la guia con condicionales Se indica como comentario que es mas eficiente o lógico usar un ciclo for ya que se sabe de antemano la cantidad de iteraciones

--- src/ar/utn/p2/tp2/ejercicios/condicionales/MayorDeTres.java
+++ src/ar/utn/p2/tp2/ejercicios/condicionales/MayorDeTres.java
@@ -1,11 +1,37 @@
 package ar.utn.p2.tp2.ejercicios.condicionales;
 
 import ar.utn.p2.tp2.core.Exercise;
+import ar.utn.p2.tp2.utils.ContextColor;
+import ar.utn.p2.tp2.utils.UtilsColor;
+
+import java.util.Scanner;
 
 public class MayorDeTres implements Exercise {
     @Override
     public void execute() {
-        System.out.print("Falta implementar");
+        int mayor;
+        Scanner scanner = new Scanner(System.in);
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese un número ", 'n');
+        int numero = scanner.nextInt();
+        mayor = numero;
 
+        // Siguiendo la guía se propone una implementación mediante condicionales
+        // Es posible realizarlo con un ciclo For ya que se sabe de ante mano las iteraciones
+
+        // Lectura y validación segundo número
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese un número ", 'n');
+        numero = scanner.nextInt();
+        if (numero > mayor) {
+            mayor = numero;
+        }
+        // Lectura y validación tercer número
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese un número ", 'n');
+        numero = scanner.nextInt();
+        if (numero > mayor) {
+            mayor = numero;
+        }
+
+        UtilsColor.imprimirEncabezado();
+        UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Número mayor ingresado "+mayor);
     }
 }
