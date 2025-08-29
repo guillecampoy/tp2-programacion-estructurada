4765f8554851e5dff7bf3a7aa6e08a56d0403c2f
Guillermo Campoy <guillecampoy@gmail.com>
2025-08-29 14:46:40 -0300

    Se implementa ejercicio 3 Determinar grupo según edad

--- src/ar/utn/p2/tp2/ejercicios/condicionales/ClasificacioEdad.java
+++ src/ar/utn/p2/tp2/ejercicios/condicionales/ClasificacioEdad.java
@@ -1,11 +1,33 @@
 package ar.utn.p2.tp2.ejercicios.condicionales;
 
 import ar.utn.p2.tp2.core.Exercise;
+import ar.utn.p2.tp2.utils.ContextColor;
+import ar.utn.p2.tp2.utils.UtilsColor;
+
+import java.util.Scanner;
 
 public class ClasificacioEdad implements Exercise {
     @Override
     public void execute() {
-        System.out.print("Falta implementar");
+        int edad;
+        UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese el edad ", 'n');
+        Scanner scanner = new Scanner(System.in);
+        edad = scanner.nextInt();
+
+        //Determinamos según la edad con bloques condicionales
+        if (edad < 12) {
+            UtilsColor.imprimirEncabezado();
+            UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Eres un niño");
+        } else if (edad <= 17) {
+            UtilsColor.imprimirEncabezado();
+            UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Eres un adolescente");
+        }  else if (edad <= 59) {
+            UtilsColor.imprimirEncabezado();
+            UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Eres un adulto");
+        } else {
+            UtilsColor.imprimirEncabezado();
+            UtilsColor.imprimirBloque(ContextColor.SUCCESS, "Eres un adulto mayor");
+        }
 
     }
 }
