ab8f4e9c5353507091af0b643460454e759f0269
Guillermo Campoy <guillecampoy@gmail.com>
2025-08-29 20:50:06 -0300

    Implemento ejercicio 8 Se utiliza dentro de la clase específica del ejercicio un método para calcular el precio final el mismo se invoca desde la clase, el método retorna el valor final calculado se hace uso de la clase Math para ajustar a dos decimales la salida del precio final

--- src/ar/utn/p2/tp2/ejercicios/funciones/CalculoImpuestoYDescuento.java
+++ src/ar/utn/p2/tp2/ejercicios/funciones/CalculoImpuestoYDescuento.java
@@ -1,11 +1,31 @@
 package ar.utn.p2.tp2.ejercicios.funciones;
 
 import ar.utn.p2.tp2.core.Exercise;
+import ar.utn.p2.tp2.utils.ContextColor;
+import ar.utn.p2.tp2.utils.UtilsColor;
+
+import java.util.Scanner;
 
 public class CalculoImpuestoYDescuento implements Exercise {
     @Override
     public void execute() {
-        System.out.print("Falta implementar");
+       double impuesto, descuento, precioBase, precioFinal;
+       Scanner scanner = new Scanner(System.in);
+       UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese precio base del producto: ", 'n');
+       precioBase = scanner.nextDouble();
+       UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese el impuesto en porcentaje (Ejemplo: 10 para 10%): ", 'n');
+       impuesto = scanner.nextDouble();
+       UtilsColor.imprimirBloque(ContextColor.INFO, "Ingrese el descuento en porcentaje (Ejemplo: 5 para 5%): ", 'n');
+       descuento = scanner.nextDouble();
 
+       // fijamos en dos decimales el resultado utilizando las funcionalidades de la clase Math
+       precioFinal = Math.round(calcularPrecioFinal(impuesto, descuento, precioBase));
+
+       UtilsColor.imprimirEncabezado();
+       UtilsColor.imprimirBloque(ContextColor.INFO, "El precio final del producto es: "+precioFinal);
+    }
+
+    double calcularPrecioFinal(double impuesto, double descuento, double precioBase) {
+        return precioBase * (1 - descuento / 100) * (1 + impuesto / 100);
     }
 }
